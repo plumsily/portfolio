a9aa02ff83072b7d5f1ece79b536b20112bd23e4
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-02-14 14:12:16 -0800

    updated styling

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function reveal() {
   for (let i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
     let elementTop = reveals[i].getBoundingClientRect().top;
-    let elementVisible = -50;
+    let elementVisible = -0;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
